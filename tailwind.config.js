a6729f03abff03bb19e922b96f40c94d22c56c75
Your Name <you@example.com>
2022-03-09 16:45:20 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'beige': '#F0E4D6',
+        'body-text': '#874A22',
+      },
+    },
+  
+  },
+  plugins: [],
+}
